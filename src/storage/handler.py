f286239c0348031a3032461fd6bc4f56dc233920
Edwin-Jan Harmsma <ejharmsma@gmail.com>
2011-09-28 16:00:11 +0200

    implemented skeleton for storage handler, not connected to storage db yet

--- src/storage/handler.py
+++ src/storage/handler.py
@@ -1,6 +1,9 @@
-from generic.communication_pb2 import HashedStorageHeader#, StorageHeader, StorageResponseHeader
+from generic.communication_pb2 import HashedStorageHeader, StorageHeader#, StorageResponseHeader
 
 from twisted.python import log
+
+from hashlib import sha1
+import time
 
 PROTOCOL_VERSION = 0b1
 
@@ -19,6 +22,7 @@ class StorageRequestHandler():
     def __init__(self, protocol):
         self.protocol = protocol
         self.db = protocol.factory.db
+        self.signedHeader = None
         
     def parsedVersionToken(self, version):
         log.msg("parsedVersionToken(%d)" % version)
@@ -29,14 +33,41 @@ class StorageRequestHandler():
         log.msg("parsedMessageLengthToken(%d)" % length)
         if length == 0:
             raise Exception("Received 0 length message")
+            
+    def _validateHash(self):
+        if self.signedHeader.hashAlgorithm != HashedStorageHeader.SHA1:
+            raise Exception("Unsupported hash")
+        sha1hash = sha1(self.signedHeader.header.SerializeToString() + PRIVATE_HASH_KEY)
+        log.msg('parsed hash: %s' % sha1hash.hexdigest())
+        if self.signedHeader.hash != sha1hash.digest():
+            raise Exception("Incorrect hash")
+        timediff = int(time.time()) - self.signedHeader.header.requestTimestamp
+        if timediff > HASH_EXPIRE_SECONDS:
+            raise Exception("Hash key expired %d seconds" % timediff)
+        
+    def _handleStorgeHeader(self):
+        opp = self.signedHeader.header.operation
+        length = self.signedHeader.header.length
+        if opp == StorageHeader.READ:
+            log.msg('Parsed READ header,  read operation')
+            return 0 # we don't want to receive any raw data
+        elif opp == StorageHeader.WRITE:
+            log.msg('Parsed WRITE header, waiting for %d bytes' % length)
+            return length
+        elif opp == StorageHeader.XOR_WRITE:
+            log.msg('Parsed XOR_WRITE header, waiting for %d bytes' % length)
+            return length
+        raise Exception("Unkown operation")
         
     def parsedMessage(self, msgData):
-        signedHeader = HashedStorageHeader()
-        signedHeader.ParseFromString(msgData)
-        log.msg("parsed message! wants to receive %d bytes" % signedHeader.header.length)
-        # TODO
-        return signedHeader.header.length
-        
+        self.signedHeader = HashedStorageHeader()
+        self.signedHeader.ParseFromString(msgData)
+        self._validateHash()
+        return self._handleStorgeHeader()
         
     def parsedRawBytes(self, bytes): # doesn't have to be implemented by dictionary service
         log.msg("Received %d raw bytes" % len(bytes))
+        # TODO: send to storage or XOR and send to storage
+        
+        
+        
