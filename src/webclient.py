df59e3c315591c1b96965364977d70f77fe7ddf2
Edwin-Jan Harmsma <ejharmsma@gmail.com>
2011-10-30 20:27:33 +0100

    finished webclient for plain text files, mime typeis not used

--- src/webclient.py
+++ src/webclient.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import cgi
+
 from os import curdir, sep
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+
+from client import store, retrieve
 
 PAGE = """
 <!DOCTYPE html>
@@ -14,12 +17,17 @@ PAGE = """
 
 <body>
     <div>
+        <p><a href="/">Back to home</a></p>
         <h1>File upload</h1>
         <div>%s</div>
         <form enctype="multipart/form-data" action="/" method="post">
-            <label for="file">File:</label>
-            <input type="file" name="file" id="file"/> 
-            <input type="submit" value="Upload" />
+            <p>
+                <label for="file">File:</label>
+                <input id="file" type="file" name="file">
+            </p>
+            <p>
+                <input type="submit" value="Upload">
+            </p>
         </form>
     </div>
 </body>
@@ -29,29 +37,54 @@ PAGE = """
 
 class MyHandler(BaseHTTPRequestHandler):
     
-    def _sendIndexPage(self, uploadedKey=""):
+    def _sendIndexPage(self, msg=""):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(PAGE % (uploadedKey))
+        self.wfile.write(PAGE % (msg))
+
+    def _sendData(self, data):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(data)
+
+    def _handleGet(self, key):
+        data = retrieve(key)
+        if data:
+            self._sendData(data)
+        else:
+            self._sendIndexPage('File Not Found: %s' % key)
 
     def do_GET(self):
         if self.path == '/':
             self._sendIndexPage()
         else:
-            self.send_error(404, 'Page Not Found: %s' % self.path)
+            self._handleGet(self.path[1:-1])
             
     def _handleFileUpload(self):
-        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-        print ctype
-        assert ctype == 'multipart/form-data'
-        query = cgi.parse_multipart(self.rfile, pdict)
-        
-        upfilecontent = query.get('upfile')
-        self._sendIndexPage("Uploaded file to: %s" % upfilecontent[0])
+        if 'file' not in self.postvars:
+            self._sendIndexPage("Nothing uploaded")
+        else:
+            fileContent = self.postvars['file'][0]
+            try:
+                key = store(fileContent)
+                self._sendIndexPage('File stored with key: <a href="/%s/">%s</a>' % (key, key))
+            except:
+                self._sendIndexPage("Error during file upload, try again...")
         
         
     def do_POST(self):
+        # parse post vars
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            self.postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers.getheader('content-length'))
+            self.postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        else:
+            self.postvars = {}
+        # select handler for path
         if self.path == '/':
             self._handleFileUpload()
         else:
